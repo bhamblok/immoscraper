855df0e351b066baae53570c3dd8e869e83ab7d0
Bob Hamblok <post@bhamblok.be>
2022-03-01 20:28:52 +0100

    start testing

---
+++ helpers/isNewImmo.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import notifyMe from './notifyMe.js';
+
+const data = JSON.parse(fs.readFileSync('./data/immoscraper.json', 'utf8'));
+
+export default async (title, content) => {
+  const newImmo = content.filter((immo) => {
+    if (!data.includes(immo.link)) {
+      data.push(immo.link);
+      fs.writeFileSync('./data/immoscraper.json', JSON.stringify(data, null, 2), 'utf8');
+      return true;
+    }
+    return false;
+  });
+  await newImmo.reduce(async (prev, immo) => {
+    await prev;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        notifyMe({
+          text: title,
+          attachment: {
+            title: immo.title,
+            title_link: immo.link,
+            text: `${immo.info} ${immo.price}`,
+            image_url: immo.image_url,
+          },
+        });
+        resolve();
+      }, 500);
+    });
+  }, Promise.resolve());
+
+  return newImmo;
+};
