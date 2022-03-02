5dc56fc75283fbe8feddd149e52e42a850c6ef50
Bob Hamblok <post@bhamblok.be>
2022-03-02 13:51:50 +0100

    add immovlan

---
+++ tests/immovlan-2018.spec.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { test, expect } from '@playwright/test';
+import scrape from '../helpers/scrape.js';
+
+const { MIN_PRICE, MAX_PRICE, MIN_SURFACE } = process.env;
+
+// https://immo.vlan.be/nl/vastgoed?transactiontypes=te-koop,in-openbare-verkoop&towns=2018-antwerpen&propertytypes=huis&minlivablesurface=170&minprice=400000&maxprice=750000&noindex=1
+
+
+test('immo test', async ({ page }) => {
+  const newImmo = await scrape({
+    page,
+    url: `https://immo.vlan.be/nl/vastgoed?transactiontypes=te-koop,in-openbare-verkoop&towns=2018-antwerpen&propertytypes=huis&minlivablesurface=${MIN_SURFACE}&minprice=${MIN_PRICE}&maxprice=${MAX_PRICE}&noindex=1`,
+    getTitle: p => p.locator('html > head > title').evaluate(el => el.innerText),
+    selectList: p => p.locator('article.list-view-item'),
+    elementHandler: async e => e.evaluate(element => ({
+      id: element.id,
+      link: element.querySelector('.card-title a').href?.split('?')[0],
+      title: element.querySelector('.card-title').innerText,
+      price: element.querySelector('.list-item-price').innerText,
+      info: element.querySelector('.list-item-description')?.innerText,
+      image_url: element.querySelector('.card-image img')?.src?.replace('-webp', ''),
+    })),
+  });
+
+  expect(newImmo.length).toBe(0);
+});
