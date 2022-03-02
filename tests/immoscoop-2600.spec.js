74ac1e3fdc705dea717f2cb686dc027005e5d662
Bob Hamblok <post@bhamblok.be>
2022-03-02 12:02:25 +0100

    add immoscoop

--- tests/immoscoop-2600.spec.js
+++ tests/immoscoop-2600.spec.js
@@ -4,12 +4,12 @@ import scrape from '../helpers/scrape.js';
 
 const { MIN_PRICE, MAX_PRICE } = process.env;
 
-// https://www.immoscoop.be/zoeken/te-koop/2000-antwerpen/all/woonhuis?minPrice=400000&maxPrice=750000&sort=date%2CDESC
+// https://www.immoscoop.be/zoeken/te-koop/2600-berchem/all/woonhuis?minPrice=400000&maxPrice=750000&sort=date%2CDESC
 
 test('immo test', async ({ page }) => {
   const newImmo = await scrape({
     page,
-    url: `https://www.immoscoop.be/zoeken/te-koop/2000-antwerpen/all/woonhuis?minPrice=${MIN_PRICE}&maxPrice=${MAX_PRICE}&sort=date%2CDESC`,
+    url: `https://www.immoscoop.be/zoeken/te-koop/2600-berchem/all/woonhuis?minPrice=${MIN_PRICE}&maxPrice=${MAX_PRICE}&sort=date%2CDESC`,
     getTitle: p => p.locator('html > head > title').evaluate(el => el.innerText),
     selectList: p => p.locator('[data-component="property-card"]'),
     elementHandler: async e => e.evaluate(element => ({
