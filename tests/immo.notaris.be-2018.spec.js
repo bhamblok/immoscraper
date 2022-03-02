32aec8ac8643a385f119b0542e2b414f45a3831c
Bob Hamblok <post@bhamblok.be>
2022-03-02 14:57:02 +0100

    add immo.notaris.be

---
+++ tests/immo.notaris.be-2018.spec.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { test, expect } from '@playwright/test';
+import scrape from '../helpers/scrape.js';
+
+const { MAX_PRICE } = process.env;
+
+// https://immo.notaris.be/nl/vastgoed-te-koop/2018-antwerpen-belgie?area=2018+Antwerpen%2C+Belgi%C3%AB&geocoding=51.2037695_4.4112637&latitude=51.2037695&longitude=4.4112637&gender=SALE&type=HOUSE&max_price=750000&radius=5
+
+test('immo test', async ({ page }) => {
+  const newImmo = await scrape({
+    page,
+    url: `https://immo.notaris.be/nl/vastgoed-te-koop/2018-antwerpen-belgie?area=2018+Antwerpen%2C+Belgi%C3%AB&geocoding=51.2037695_4.4112637&latitude=51.2037695&longitude=4.4112637&gender=SALE&type=HOUSE&max_price=${MAX_PRICE}&radius=5`,
+    getTitle: p => p.locator('html > head > title').evaluate(el => el.innerText),
+    selectList: p => p.locator('.property__item'),
+    elementHandler: async e => e.evaluate(element => ({
+      link: element.querySelector('a').href?.split('?')[0],
+      title: element.querySelector('.property__item--textlink .h3').innerText,
+      price: element.querySelector('.property__item--price').innerText,
+      info: element.querySelector('.property__item--textlink .h5')?.innerText,
+      image_url: element.querySelector('.container--img img')?.src?.replace('-webp', ''),
+    })),
+  });
+
+  expect(newImmo.length).toBe(0);
+});
