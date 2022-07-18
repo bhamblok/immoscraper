74da0ad1ebac7710a95d83705b4169b284101792
Bob Hamblok <post@bhamblok.be>
2022-07-18 14:51:11 +0200

    update zimmo url

--- tests/zimmo.spec.js
+++ tests/zimmo.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import scrape from '../helpers/scrape.js';
 
 const title = 'Zimmo';
-const url = 'https://www.zimmo.be/nl/zoeken/?search=eyJmaWx0ZXIiOnsic3RhdHVzIjp7ImluIjpbIkZPUl9TQUxFIl19LCJwbGFjZUlkIjp7ImluIjpbMzI3MCwzMjcxLDYwMl19LCJjYXRlZ29yeSI6eyJpbiI6WyJIT1VTRSJdfSwicHJpY2UiOnsidW5rbm93biI6dHJ1ZSwicmFuZ2UiOnsibWluIjozMDAwMDAsIm1heCI6ODAwMDAwfX19fQ%3D%3D#gallery';
+const url = 'https://www.zimmo.be/nl/zoeken/?search=eyJmaWx0ZXIiOnsic3RhdHVzIjp7ImluIjpbIkZPUl9TQUxFIl19LCJwbGFjZUlkIjp7ImluIjpbMzI3MCwzMjcxLDYwMl19LCJjYXRlZ29yeSI6eyJpbiI6WyJIT1VTRSJdfSwicHJpY2UiOnsidW5rbm93biI6dHJ1ZSwicmFuZ2UiOnsibWluIjozMDAwMDAsIm1heCI6ODAwMDAwfX19LCJwYWdpbmciOnsiZnJvbSI6MCwic2l6ZSI6MTd9LCJzb3J0aW5nIjpbeyJ0eXBlIjoiREFURSIsIm9yZGVyIjoiREVTQyJ9XX0%3D&p=1#gallery';
 
 test(title, async ({ page }) => {
   const newImmo = await scrape({
