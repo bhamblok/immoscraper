5a54476c471a225c98f6fdaf2de2bd866167cd9c
Bob Hamblok <post@bhamblok.be>
2022-08-16 09:49:15 +0200

    update immoscoop url having "primeur" sorting

--- tests/immoscoop.spec.js
+++ tests/immoscoop.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import scrape from '../helpers/scrape.js';
 
 const { MIN_PRICE, MAX_PRICE } = process.env;
-const url = `https://www.immoscoop.be/zoeken/te-koop/2000-antwerpen,2018-antwerpen,2600-berchem/alle/woonhuis?minPrice=${MIN_PRICE}&maxPrice=${MAX_PRICE}&sort=date%2CDESC`;
+const url = `https://www.immoscoop.be/zoeken/te-koop/2000-antwerpen,2018-antwerpen,2600-berchem/alle/woonhuis?minPrice=${MIN_PRICE}&maxPrice=${MAX_PRICE}&sort=scoop%2CDESC%7Cdate%2CDESC`;
 const title = 'Immoscoop';
 
 test(title, async ({ page }) => {
