c0c9e097508edeadb9bb884cdc57d4689b49b31e
Bob Hamblok <post@bhamblok.be>
2022-08-18 12:41:38 +0200

    update deboerenpartners URL

--- helpers/notifyMe.js
+++ helpers/notifyMe.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-console */
 
 import 'dotenv/config'; // eslint-disable-line import/no-extraneous-dependencies
-import { WebClient, LogLevel } from '@slack/web-api';
+import { WebClient } from '@slack/web-api';
 
 const { SLACK_TOKEN, SLACK_CHANNEL_ID } = process.env;
 
-const client = new WebClient(SLACK_TOKEN, { logLevel: LogLevel.DEBUG });
+const client = new WebClient(SLACK_TOKEN);
 
 export default async ({ title, immo }) => {
   console.log('notify:', immo.link);
   try {
     const result = await client.chat.postMessage({
       channel: SLACK_CHANNEL_ID,
-      text: `${title}, ${immo.title} - ${immo.info} ${immo.price ?? '(waarschijnlijk reeds verkocht)'}
+      text: `[${title}] ${immo.title} - ${immo.info} ${immo.price ?? '(waarschijnlijk reeds verkocht)'}
 ${immo.link}`,
     });
     console.log(result);
